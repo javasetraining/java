f99ecae669c70324668560b838ff1ba3c90083ce
tkhorik <marvelmd@gmail.com>
2015-01-20 23:30:45 +0200

    commit files from two lectures

---
+++ src/demovariabilemetodeclase/DemoVariabileMetodeClase.java
@@ -0,0 +1,36 @@
+
+package demovariabilemetodeclase;
+
+/**
+ *
+ * @author iurasun
+ */
+public class DemoVariabileMetodeClase {
+
+    //var primitive  : byte, short, int, long ;  float, double ; boolean; void ; char ca short
+    int x=7;
+    static int y;
+
+
+    //var Referinte
+    //  Byte, Short, Integer, Long   , Float, Double, , Boolean Void, Character
+    // wrapper
+
+    String s="Hello";
+    static String ss;
+    A aRef= new A();
+
+
+    public static void main(String[] args) {
+
+        int d=55;
+        int ds=99;
+
+        String sss="dgfasdfs";
+        A aa=new A();
+
+    }
+
+}
+
+
