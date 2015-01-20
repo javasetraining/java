bb7a0e057f1ad0b7ab2b3b1cbe450984fe2ea2af
tkhorik <marvelmd@gmail.com>
2015-01-20 23:36:38 +0200

    clear repo

--- src/demovariabilemetodeclase/A.java
+++
@@ -1,9 +0,0 @@
-package demovariabilemetodeclase;
-
-
-public class A {
-
-    public           int aMic ;
-    public   static  int aMicStatic =99;
-
-}
--- src/mySrc/HelloLume.java
+++
@@ -1,9 +0,0 @@
-package mySrc;
-
-import javax.swing.*;
-
-public class HelloLume{
-    public static void main (String[] args){
-        System.out.println("qwe");
-    }
-}
