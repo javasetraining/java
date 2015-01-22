4957f810cd8b67150812aa4f4c97611fa6ef7aa0
tkhorik <marvelmd@gmail.com>
2015-01-23 00:27:01 +0200

    my home work first try from home

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SE
+here keep your home work notes and tasks
+you should be prepared for every of them
+
--- src/DemoJavaBasics/TestCar.java
+++ src/DemoJavaBasics/TestCar.java
@@ -5,11 +5,26 @@ package DemoJavaBasics;
  */
 public class TestCar {
     public static void main(String[] args) {
-        Car firstCar = new Car(4,100,1);
-        Car secondCar = new Car(4,200,2);
+        Car dacia = new Car(4,100,1);
+        Car ferary = new Car(5,200,2);
 
 
 //        zakon4iti kak i avion i razabratisea!!!
+//==============================================================
+        System.out.println("auto for every day " +dacia.toString());
+        System.out.println("auto for ricers "+ferary.toString());
+
+
+        System.out.println("after tuning moldavskii : ");
+        dacia.setPutereaMotorului(130);
+        dacia.setWheelsNumber(5);
+        dacia.setCarCost(100);
+        System.out.println("Dacia after tuning "+dacia.toString());
+
+        ferary.setPutereaMotorului(350);
+        ferary.setWheelsNumber(4);
+        ferary.setCarCost(70000);
+        System.out.println("Ferary arter reparation"+ferary.toString());
 
     }
 }
