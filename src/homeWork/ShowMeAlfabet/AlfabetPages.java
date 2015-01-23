48d8eb92ae33e2e9bd63ca5ab1b54e30e6390308
DimaAvantaj <dt@avantajprim.com>
2015-01-23 17:24:10 +0200

    little changes

---
+++ src/homeWork/ShowMeAlfabet/AlfabetPages.java
@@ -0,0 +1,10 @@
+package homeWork.ShowMeAlfabet;
+
+/**
+ * Created by Dmitry on 1/23/2015.
+ */
+public class AlfabetPages {
+
+
+
+}
---
+++ src/homeWork/ShowMeAlfabet/ShowMeAlfabet.java
@@ -0,0 +1,16 @@
+package homeWork.ShowMeAlfabet;
+
+/**
+ * Created by MaRV on 1/20/2015.
+ */
+public class ShowMeAlfabet {
+//  String inputs = "qweqwe";
+//                         inputs - new inputs = 32;
+//
+//    public JComponent[] inputs;
+//     //Массив отображаемых компонет
+//    int reply = JOptionPane.showConfirmDialog(null, inputs, "Заголовок", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+//    if(reply==JOptionPane.OK_OPTION)
+//}
+//
+}
